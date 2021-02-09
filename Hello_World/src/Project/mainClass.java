35c3eabdddad70698844bc1900d44bdce86c21ed
lynxl <lynxl@DESKTOP-G638I1A.home>
2021-02-09 17:53:44 -0500

    Initial Commit

---
+++ Hello_World/src/Project/mainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Project;
+
+/**
+ * @author lynxl
+ *
+ */
+public class mainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Hello World");
+	}
+
+}
